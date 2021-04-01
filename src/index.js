c91571d2e32fff39fd819029c9dde2a43d285d89
Brian Hook <34469882+BrianHook1183@users.noreply.github.com>
2021-04-01 12:19:50 -0500

    initial commit | qualified prompt

---
+++ src/index.js
@@ -0,0 +1,7 @@
+//! import "./index.css";
+//! probably should have been /src/index.css but wasn't working locally so I added css link to index.html
+
+
+//* To restart this challenge, delete everything below. Everything but this file was given to me (and I already moved css link to index.html to fix qualified vs local compatibility issue). 
+
+
